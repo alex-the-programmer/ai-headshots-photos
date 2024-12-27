92fc3e8cd2b9c62c48e93d9a153d68fa6d2f2823
Alex Marchenko <AlexTheProgrammer@hotmail.com>
2024-12-26 19:14:53 -0800

    formatting day

--- components/projectsList/projectCard.tsx
+++ components/projectsList/projectCard.tsx
@@ -1,9 +1,9 @@
-import { Project } from "@/types/project";
 import { ProjectsNavigationProp } from "@/types/navigation";
 import { Text, StyleSheet } from "react-native";
 import IntoCard from "@/components/common/intoCard";
 import { gql } from "@apollo/client";
 import { ProjectCardFragment } from "@/generated/graphql";
+import dayjs from "dayjs";
 
 const ProjectCard = ({
   project,
@@ -25,7 +25,9 @@ const ProjectCard = ({
       onPress={handlePress}
       thumbnails={[project.projectPhotoUrl || ""]}
     >
-      <Text style={styles.whiteText}>{project.createdAt}</Text>
+      <Text style={styles.whiteText}>
+        {dayjs(project.createdAt).format("MM/DD/YYYY")}
+      </Text>
       <Text style={styles.whiteText}>{project.processingStatus}</Text>
     </IntoCard>
   );
